82ccd15a7c1f22680dacc66e2f96cd65c904432f
Dante Vicenzo <dantevcz@gmail.com>
2023-05-20 04:36:46 -0300

    Formated date

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,6 +1,8 @@
 import { useContext } from 'react'
 import { HistoryContainer, Status, StyledTable } from './styles'
 import { CyclesContext } from '../../contexts/CyclesContext'
+import { formatDistanceToNow } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
 
 export function History() {
   const { cycles } = useContext(CyclesContext)
@@ -21,8 +23,13 @@ export function History() {
           {cycles.map((cycle) => (
             <tr key={cycle.id}>
               <td>{cycle.task}</td>
-              <td>{cycle.minutesAmount}</td>
-              <td>{cycle.startDate.toDateString()}</td>
+              <td>{cycle.minutesAmount} minutos</td>
+              <td>
+                {formatDistanceToNow(cycle.startDate, {
+                  locale: ptBR,
+                  addSuffix: true,
+                })}
+              </td>
               <td>
                 {cycle.status === 'completed' && (
                   <Status statusColor="completed">Concluído</Status>
